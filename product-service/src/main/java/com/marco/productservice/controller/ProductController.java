f4dd5a1e2ff16e95a3816264d4de7c881232331f
Hermon Weldegiorgis <hermon.weldegiorgis@gmail.com>
2024-04-20 15:27:28 -0500

    product-service-1
    Initial commit of product-service with a test case for creating a product.

---
+++ product-service/src/main/java/com/marco/productservice/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.marco.productservice.controller;
+
+import com.marco.productservice.dto.ProductRequest;
+import com.marco.productservice.dto.ProductResponse;
+import com.marco.productservice.model.Product;
+import com.marco.productservice.repository.ProductRepository;
+import com.marco.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest){
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
